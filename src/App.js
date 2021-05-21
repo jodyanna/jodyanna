a3427dd580ca5bea33b94bd50c24cb2f648d96c6
joseph danna <jodyanna14.15@gmail.com>
2021-05-20 23:12:03 -0500

    laying some groundwork

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <div className="page">
+
+        <Switch>
+          <Route path="/about">
+
+          </Route>
+          <Route path="/users">
+
+          </Route>
+          <Route path="/">
+            <Welcome />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Welcome/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './index.module.css';
+
+export default function Welcome() {
+
+  return (
+    <div className={styles.container}>
+      <h1>Hi friend!</h1>
+      <p>What brings you here?</p>
+
+      <div>
+
+      </div>
+    </div>
+  )
+}
